2862371e389ee755afa797f2820581a6830cf537
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-11-21 23:50:22 -0500

    prepared for deploy

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 "use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const morgan = require("morgan");
 
 const routes = require("./routes");
 
 const PORT = 4000;
+
+const buildPath = path.join(__dirname, "..", "client/build");
+
+console.log("Build path is", buildPath);
 
 express()
   .use(function (req, res, next) {
@@ -20,10 +25,15 @@ express()
     next();
   })
   .use(morgan("tiny"))
-  .use(express.static("./server/assets"))
+  // .use(express.static("./server/assets"))
+  .use(express.static(buildPath))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
   .use("/", express.static(__dirname + "/"))
   .use("/", routes)
+
+  .get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  })
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
